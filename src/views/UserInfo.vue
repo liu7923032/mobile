062a150585014bdabef8edb4c2312c77074ad7a4
liu7923032 <1137411088@qq.com>
2016-01-07 14:54:52 +0800

    add user

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,35 @@
+<template>
+	<toolbar text="个人主页">
+		<span class="icon-chevron-left" slot="leftBar">返回</span>
+	</toolbar>
+	<div>
+		
+	</div>
+</template>
+
+
+<script lang="babel">
+	
+	import ToolBar from '../components/ToolBar.vue'
+	export default {
+		created(){
+
+		},
+		props:{
+
+		},
+		methods:{
+
+		},
+		components:{
+			toolbar:ToolBar
+		}
+
+	}
+
+
+</script>
+
+<style type="text/css">
+	
+</style>
